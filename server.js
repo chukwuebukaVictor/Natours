328a313c3cf56c461466c2392531fb005ed74627
Chukwuebuka <chukwuebuka.victor.ozoede@gmail.com>
2022-09-04 21:30:43 +0100

    Connect database to nodeJs

--- server.js
+++ server.js
@@ -1,9 +1,39 @@
+const mongoose = require('mongoose');
+
 const dotenv = require('dotenv');
 const app = require('./app');
 
 dotenv.config({ path: './config.env' });
-
 // console.log(process.env)
+
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+//******LOCAL DB*********
+// mongoose
+//   .connect(process.env.DATABASE_LOCAL)
+//   .then(() => console.log('DB connections successful!'));
+
+mongoose.connect(DB).then(() => console.log('DB connections successful!'));
+
+const tourSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'A tour must have a name'],
+    unique: true,
+  },
+  rating: {
+    type: Number,
+    default: 4.5,
+  },
+  price: {
+    type: Number,
+    required: [true, 'A tour must have a price'],
+  },
+});
+
+const Tour = mongoose.model('Tour', tourSchema);
 
 const port = process.env.PORT || 8000;
 app.listen(port, () => {
