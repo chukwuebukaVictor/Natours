5ca181276c4cfd4904180006c1f89c567ca29fc0
Chukwuebuka <chukwuebuka.victor.ozoede@gmail.com>
2022-09-09 12:58:15 +0100

    Implement aggregation pipelining

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -114,11 +114,40 @@ exports.deleteTour = async (req, res) => {
   }
 };
 
-//{difficulty: 'easy', duration:{$gte: 5}}
-//{ difficulty: 'easy', duration: { gte: '5' } }
-
-// const query = Tour.find()
-//   .where('duration')
-//   .equals(5)
-//   .where('difficulty')
-//   .equals('easy');
+exports.getTourStats = async (req, res) => {
+  try {
+    const stats = await Tour.aggregate([
+      {
+        $match: { ratingsAverage: { $gte: 4.5 } },
+      },
+      {
+        $group: {
+          _id: { $toUpper: '$difficulty' },
+          numTours: { $sum: 1 },
+          numRatings: { $sum: '$ratingsQuantity' },
+          avgRating: { $avg: '$ratingsAverage' },
+          avgPrice: { $avg: '$price' },
+          minPrice: { $min: '$price' },
+          maxPrice: { $max: '$price' },
+        },
+      },
+      {
+        $sort: { avgPrice: 1 },
+      },
+      // {
+      //   $match: { _id: { $ne: 'EASY' } },
+      // },
+    ]);
+    res.status(200).json({
+      status: 'success',
+      data: {
+        stats,
+      },
+    });
+  } catch (err) {
+    res.status(404).json({
+      status: 'fail',
+      message: err,
+    });
+  }
+};
