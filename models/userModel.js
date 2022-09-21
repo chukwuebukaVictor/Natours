cc18580301f25cf3b256cda0817bc168574075f6
Chukwuebuka <chukwuebuka.victor.ozoede@gmail.com>
2022-09-21 16:42:36 +0100

    Setup post request for user signup

---
+++ models/userModel.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+const validator = require('validator');
+const bcrypt = require('bcryptjs');
+
+const userSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, 'Please tell us your name'],
+    // unique: true,
+    // trim: true,
+    // minLength: [10, 'User name must be more or equal to 10 characters'],
+    // maxLength: [40, 'User name must be less or equal to 40 characters'],
+  },
+  email: {
+    type: String,
+    required: [true, 'Please provide your email'],
+    unique: true,
+    lowerCase: true,
+    validate: [validator.isEmail, 'Please provide a valid email'],
+  },
+  photo: String,
+  password: {
+    type: String,
+    required: [true, 'Please provide a password'],
+    minlength: 8,
+  },
+  passwordConfirm: {
+    type: String,
+    required: [true, 'Please confirm your password'],
+    validate: {
+      //This only works on CREATE and SAVE
+      validator: function (el) {
+        return el === this.password;
+      },
+      message: 'Passwords are not the same!',
+    },
+  },
+});
+
+userSchema.pre('save', async function (next) {
+  //Only run this function if password was actually modified
+  if (!this.isModified('password')) return next();
+
+  //Hash the passord with cost of 12
+  this.password = await bcrypt.hash(this.password, 12);
+
+  //Delete passwordConfirm field
+  this.passwordConfirm = undefined;
+  next();
+});
+
+const User = mongoose.model('User', userSchema);
+
+module.exports = User;
