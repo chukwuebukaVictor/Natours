e86cfd76512fe1bd12dd2c1ae9b46fa4b9bfe849
Chukwuebuka <chukwuebuka.victor.ozoede@gmail.com>
2022-09-22 21:36:41 +0100

    Setup for user creation

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,13 +1,17 @@
 const jwt = require('jsonwebtoken');
 const User = require('../models/userModel');
 const catchAsync = require('../utils/catchAsync');
+const AppError = require('../utils/appError');
+
+const signToken = (id) =>
+  jwt.sign({ id }, process.env.JWT_SECRET, {
+    expiresIn: process.env.JWT_EXPIRES_IN,
+  });
 
 exports.signup = catchAsync(async (req, res, next) => {
   const newUser = await User.create(req.body);
 
-  const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
-    expiresIn: process.env.JWT_EXPIRES_IN,
-  });
+  const token = signToken(newUser._id);
 
   res.status(201).json({
     status: 'success',
@@ -16,4 +20,51 @@ exports.signup = catchAsync(async (req, res, next) => {
       user: newUser,
     },
   });
+});
+
+exports.login = catchAsync(async (req, res, next) => {
+  const { email, password } = req.body;
+
+  //Check if email and password exist
+  if (!email || !password) {
+    return next(new AppError('Please provide email and password', 400));
+  }
+  //Check if user exist and password is correct
+  const user = await User.findOne({ email }).select('+password');
+  //   console.log(user);
+
+  if (!user || !(await user.correctPassword(password, user.password))) {
+    return next(new AppError('Incorrect email or password!', 401));
+  }
+  //if everything is ok, send token to client
+  const token = signToken(user._id);
+  res.status(200).json({
+    status: 'success',
+    token,
+  });
+});
+
+exports.protect = catchAsync(async (req, res, next) => {
+  //Get the token and check if it is there
+  let token;
+  if (
+    req.headers.authorisation &&
+    req.headers.authorisation.startsWith('Bearer')
+  ) {
+    token = req.headers.authorisation.split(' ')[1];
+  }
+  //   console.log(token);
+
+  if (!token) {
+    return next(
+      new AppError('You are not logged in! Please log in to get access', 401)
+    );
+  }
+  //Token verification
+
+  //Check if user still exists
+
+  //Check if user changed password after the token was issued
+
+  next();
 });
--- controllers/userController.js
+++ controllers/userController.js
@@ -1,9 +1,20 @@
-const getAllUsers = (req, res) => {
-  res.status(505).json({
-    status: 'error',
-    message: 'This route is not yet defined',
+const User = require('../models/userModel');
+
+const catchAsync = require('../utils/catchAsync');
+
+const getAllUsers = catchAsync(async (req, res, next) => {
+  const users = await User.find();
+
+  //SEND RESPONSE
+  res.status(200).json({
+    status: 'success',
+    // console.log(req.requestTime);
+    result: users.length,
+    data: {
+      users,
+    },
   });
-};
+});
 
 const createUser = (req, res) => {
   res.status(505).json({
