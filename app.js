20faf3d44cf2048e49df7aaac2a09b6cd43cdddb
Chukwuebuka <chukwuebuka.victor.ozoede@gmail.com>
2022-10-04 16:33:49 +0100

    Implement middlewares for limit request from same API and to set security http headers

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-// const fs = require('fs');
 const express = require('express');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
+
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
 
@@ -8,17 +10,32 @@ const app = express();
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
-//Middlewares
+//Global Middlewares
+//Set security Http headers
+
+app.use(helmet());
 // console.log(process.env.NODE_ENV)
+//
 if (process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-if (process.env.NODE_ENV === 'production') {
-  console.log('Hello prod');
-}
-app.use(express.json());
+
+//Limit request from same API
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 1000,
+  message: 'Too many request from this IP, please try again in an hour',
+});
+
+app.use('/api', limiter);
+
+//Body parser, reading data from the body into req.body
+app.use(express.json({ limit: '10kb' }));
+
+//Serving static files
 app.use(express.static(`${__dirname}/public`));
 
+//Test middleware
 app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   // console.log(req.headers);
--- controllers/authController.js
+++ controllers/authController.js
@@ -14,6 +14,20 @@ const signToken = (id) =>
 const createSendToken = (user, statusCode, res) => {
   const token = signToken(user._id);
 
+  const cookieOptions = {
+    expires: new Date(
+      Date.now() + process.env.JWT_cookie_EXPIRES_IN + 24 + 60 + 60 + 1000
+    ),
+
+    httpOnly: true,
+  };
+  if (process.env.NODE_ENV === 'production') cookieOptions.secure = true;
+
+  res.cookie('jwt', token);
+
+  //Remove the password output
+  user.password = undefined;
+
   res.status(statusCode).json({
     status: 'success',
     token,
