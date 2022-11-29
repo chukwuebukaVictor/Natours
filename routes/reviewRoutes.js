2d835e46a4a2d7342e6d70e8adfb665a0fb00cc8
Chukwuebuka Victor <chukwuebuka.victor.ozoede@gmail.com>
2022-11-29 01:05:28 +0100

    Refactor of authentication and authorisation

--- routes/reviewRoutes.js
+++ routes/reviewRoutes.js
@@ -12,16 +12,19 @@ const authController = require('../controllers/authController');
 //To have access to the nexted parameter in tour routes
 const router = express.Router({ mergeParams: true });
 
+router.use(authController.protect)
+
 router
   .route('/')
   .get(getAllReviews)
   .post(
-    authController.protect,
     authController.restrictTo('user'),
     setTourUsersIds,
     createReview
   );
 
-router.route('/:id').patch(updateReview).delete(deleteTour).get(getReview);
+router.route('/:id').patch(authController.restrictTo('user', 'admin'), 
+updateReview).delete(authController.restrictTo('user', 'admin'),
+deleteTour).get(getReview);
 
 module.exports = router;
--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -19,15 +19,20 @@ router.post('/login', authController.login);
 
 router.post('/forgotPassword', authController.forgotPassword);
 router.patch('/resetPassword/:token', authController.resetPassword);
+
+//Protect all route after this middleware
+router.use(authController.protect)
+
 router.patch(
   '/updateMyPassword',
-  authController.protect,
   authController.updatePassword
 );
 
-router.get('/me', authController.protect, getMe, getUser);
-router.patch('/updateMe', authController.protect, updateMe);
-router.delete('/deleteMe', authController.protect, deleteMe);
+router.get('/me',getMe, getUser);
+router.patch('/updateMe', updateMe);
+router.delete('/deleteMe', deleteMe);
+
+router.use(authController.restrictTo('admin'))
 
 router.route('/').get(getAllUsers).post(createUser);
 router.route('/:id').get(getUser).patch(updateUser).delete(deleteUser);
